472dbc55bc818c0ae72a3e19553e1e8e82bd4240
VP <vladislavponomaryov96@gmail.com>
2022-08-31 13:00:37 +0300

    create-my-react-app

---
+++ react-kabzda-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png" alt="Logotype"/>
+        </header>
+    )
+}
+
+export default Header;
