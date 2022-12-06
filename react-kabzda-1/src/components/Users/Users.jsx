81fadf58c590b079daaba3373dc6f0933413e5fd
vladislavponomaryov <vladislavponomaryov96@gmail.com>
2022-12-06 12:43:23 +0600

    53. Классовый компонент (extends React.Component)

--- react-kabzda-1/src/components/Users/Users.jsx
+++ react-kabzda-1/src/components/Users/Users.jsx
@@ -1,45 +1,46 @@
+import React from "react";
 import s from './Users.module.css';
 import userPhoto from '../../assets/images/avatar.png';
 import axios from "axios";
 
-let Users = (props) => {
-
-    let getUsers = () => {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         if (props.users.length === 0) {
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 props.setUsers(response.data.items);
             });
         }
     }
-
-    let users = props.users.map(u => <div className={s.user}>
-        <div className={s.userLeft}>
-            <img src={userPhoto} alt="Avatar" className={s.avatar}/>
-            {u.followed ? <button onClick={() => {
-                props.unfollow(u.id)
-            }}>Unfollow</button> : <button onClick={() => {
-                props.follow(u.id)
-            }}>Follow</button>}
-        </div>
-        <div className={s.userMain}>
+    render() {
+        return <div>
+            <h3>Users</h3>
             <div>
-                <span>{u.name}</span>
-                <span>{u.status}</span>
-            </div>
-            <div>
-                {/*{u.location.country}*/}Belarus
-                {/*{u.location.city}*/}Minsk
+                {
+                    this.props.users.map(u => <div className={s.user}>
+                    <div className={s.userLeft}>
+                        <img src={userPhoto} alt="Avatar" className={s.avatar}/>
+                        {u.followed ? <button onClick={() => {
+                            this.props.unfollow(u.id)
+                        }}>Unfollow</button> : <button onClick={() => {
+                            this.props.follow(u.id)
+                        }}>Follow</button>}
+                    </div>
+                    <div className={s.userMain}>
+                        <div>
+                            <span>{u.name}</span>
+                            <span>{u.status}</span>
+                        </div>
+                        <div>
+                            {/*{u.location.country}*/}Belarus
+                            {/*{u.location.city}*/}Minsk
+                        </div>
+                    </div>
+                </div>)
+                }
             </div>
         </div>
-    </div>)
-
-    return <div>
-        <h3>Users</h3>
-        <button onClick={getUsers}>Get users</button>
-        <div>
-            {users}
-        </div>
-    </div>
+    }
 }
 
 export default Users;
