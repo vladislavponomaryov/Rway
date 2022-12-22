4e7680b041f69f02719109ff95eab32cd007d8be
vladislavponomaryov <vladislavponomaryov96@gmail.com>
2022-12-22 17:22:48 +0600

    75. Redux-form введение (login)

--- react-kabzda-1/src/components/Login/Login.jsx
+++ react-kabzda-1/src/components/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import ReduxForm from "redux-form/lib/reduxForm";
+import Field from "redux-form/lib/Field";
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'Login'} name={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field placeholder={'Password'} name={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field type="checkbox" name={'rememberMe'} component={'input'}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = ReduxForm({form: 'login'})(LoginForm);
 
 const Login = () => {
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     )
 }
