aab188f4ffde9e25a42de889d65e2d9f4bb78090
vladislavponomaryov <vladislavponomaryov96@gmail.com>
2022-12-24 14:53:20 +0300

    77. Redux-form. field validation. part 1

--- react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Post from "./Post/Post";
 import Field from "redux-form/lib/Field";
 import ReduxForm from "redux-form/lib/reduxForm";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {Textarea} from "../../common/FormsControls/FormsControls";
+
+const maxLength5 = maxLengthCreator(30);
 
 const AddPostForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'New post'} name={'newPostBody'} component={'textarea'}/>
+                <Field placeholder={'New post'} name={'newPostBody'} component={Textarea} validate={[required,maxLength5]}/>
             </div>
             <div>
                 <button>Add post</button>
